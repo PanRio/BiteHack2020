2a5d2f832da81731d7e44591f6ab081ffa57f9ba
jozin1 <jedrzej.minda@gmail.com>
2020-01-11 20:30:00 +0100

    sterowanie mega

---
+++ sketch_jan11a/sketch_jan11a.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  Servo myservoX;
+  Servo myservoY;
+
+  int setpointX;
+  int setpointY;
+
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
