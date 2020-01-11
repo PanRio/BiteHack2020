cccd2a683d07ecaf04596bf3bbf6cc728176fcb3
jozin1 <jedrzej.minda@gmail.com>
2020-01-11 21:46:34 +0100

    zmiany

--- sketch_jan11a/sketch_jan11a.ino
+++ sketch_jan11a/sketch_jan11a.ino
@@ -1,16 +1,84 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+
   Servo myservoX;
   Servo myservoY;
+  Servo silnikL;
+  Servo silnikR;
 
-  int setpointX;
-  int setpointY;
+  int x;
+  int y;
+  char msg;
+  String c = "";
+  char x_char;
+  char y_char;
+  char nextChar = 0;
+  char nextChar = 0;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  
+
+  myservoX.attach(2);
+  myservoY.attach(3);
+  silnikL.attach(4);
+  silnikR.attach(5);
 
   
+  
+  
+  Serial.begin(115200);
 
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
 
+  myservoX.write(2137);
+  if (Serial.available()) 
+  {
+    msg = Serial.read();
+    
+  }
+
+}
+
+void odczyt(char msg)
+{
+  if(msg != "$" && msg != 0)
+  {
+    c += msg;
+  }
+  else
+  {
+    for(int i = 0; i < c.length(); i++)
+    {
+      if(c[i] == "@")
+      {
+        nextChar = "x";
+      }
+      else if(c[i] == "#")
+      {
+        nextChar = "y";
+      }
+      else
+      {
+        if(nextChar = "x")
+        {
+          x_char += msg;
+          nextChar = 0;
+        }
+        if(nextChar = "y")
+        {
+          y_char += msg;
+          nextChar = "";
+        }
+      }
+    }
+    x = toInt(x_char);
+    y = toInt(y_char);
+    c = 0;
+  }
+  return 0;
 }
