84b935267807e64ecee7fcd980e107856a9fbce9
jozin1 <jedrzej.minda@gmail.com>
2020-01-12 02:22:12 +0100

    PID - bez bang bang controll :(

--- sketch_jan11a/sketch_jan11a.ino
+++ sketch_jan11a/sketch_jan11a.ino
@@ -1,30 +1,43 @@
 #include <Servo.h>
+#include <PID_v1.h>
+#include <stdio.h>
 
+//silniki i serwa
   Servo myservoX;
   Servo myservoY;
   Servo silnikL;
   Servo silnikR;
 
+//aktualna pozycja
   int x;
   int y;
+
   char msg;
   String c = "";
   String x_char = "";
   String y_char = "";
+  String header = "";
   char nextChar = 0;
 
+//PID
+  double SetpointX, InputX, OutputX;
+  PID myPIDx(&InputX, &OutputX, &SetpointX,0.02,0.05,0.001, DIRECT);
 
 void setup() 
 {
   // put your setup code here, to run once:
   
 
-  myservoX.attach(2);
-  myservoY.attach(3);
-  silnikL.attach(4);
-  silnikR.attach(5);
+  myservoX.attach(3);
+  myservoY.attach(5);
+  silnikL.attach(6);
+  silnikR.attach(9);
 
-  
+//punkt pracy itp
+  //InputX = analogRead(0);
+  SetpointX = 350;
+
+  myPIDx.SetMode(AUTOMATIC);
   
   
   Serial.begin(115200);
@@ -35,50 +48,70 @@ void loop()
 {
   // put your main code here, to run repeatedly:
 
-  myservoX.write(2137);
+  
   if (Serial.available()) 
   {
-    msg = Serial.read();
-    
+    odczyt(Serial.read());
+    Serial.print("x= ");
+    Serial.print(x);
+    Serial.print(" y= ");
+    Serial.println(y);
   }
 
+  InputX = x;
+  myPIDx.Compute();
+  myservoX.write(OutputX);
 }
+
+
 
 void odczyt(char msg)
 {
-  if(msg != "$" && msg != 0)
-  {
-    c += msg;
-  }
-  else
-  {
-    for(int i = 0; i < c.length(); i++)
-    {
-      if(c[i] == "@")
-      {
-        nextChar = "x";
-      }
-      else if(c[i] == "#")
-      {
-        nextChar = "y";
-      }
-      else
-      {
-        if(nextChar = "x")
-        {
-          x_char += msg;
-          nextChar = 0;
+               // read a byte, then
+        Serial.println(msg);
+        //Serial.println(c);
+        header += msg;
+        if(msg=='c'){
+         bool startX=false;
+         bool startY=false;
+
+
+          for(int i=0;i<header.length();i++)
+          {
+             if(header[i]=='a')
+             {
+                startX=true;
+                i++;
+             } 
+             else if(header[i]=='b')
+             {
+                startX=false;
+                startY=true;
+                i++;
+             } 
+             
+              else if(header[i]=='c')
+             {
+                header="";
+                break;
+             }  
+
+              if(startX){
+                x_char+=header[i];
+              }
+              else if(startY){
+                y_char+=header[i];             
+              }
+              
+                                                                                                     
+          }
+
+
+         x=x_char.toInt();
+         y=y_char.toInt();
+    
+          x_char="";
+          y_char="";
+           
         }
-        if(nextChar = "y")
-        {
-          y_char += msg;
-          nextChar = 0;
-        }
-      }
-    }
-    x = x_char.toInt();
-    y = y_char.toInt();
-    c = "";
-  }
-  return 0;
 }
